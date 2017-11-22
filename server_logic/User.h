f7b37fc0e62d6ae5204833209e81ac23937fde0a
Alexandre Dreyfuss <arele94@gmail.com>
2017-11-22 12:23:46 +0200

    add User interface

--- server_logic/User.h
+++ server_logic/User.h
@@ -1,11 +1,21 @@
 #pragma once
 #include <iostream>
 using namespace std;
-
+/*
+	User class with name and if hes currently banned or not.
+*/
 namespace CyberServer{
 
 	class User {
 		bool banned;
 		std::string username;
+	public:
+		User(std::string username);
+		User();
+		bool isBanned();
+		bool unBan();
+		bool Ban();
+		std::string getName();
+		bool setName(std::string username);
 	};
 }
